ac476a3334ef31c21b98b527d8bdee4a7643f7ab
Benjamin Vanlalvena <vanlalvena@outlook.com>
2024-07-22 01:49:11 +0530

    Quartz sync: Jul 22, 2024, 1:49 AM

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -1,26 +1,5 @@
-import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
-import style from "./styles/footer.scss"
-import { version } from "../../package.json"
-import { i18n } from "../i18n"
 
-interface Options {
-  links: Record<string, string>
-}
-
-export default ((opts?: Options) => {
-  const Footer: QuartzComponent = ({ displayClass, cfg }: QuartzComponentProps) => {
-    const year = new Date().getFullYear()
-    const links = opts?.links ?? []
-    return (
-      <footer class={`${displayClass ?? ""}`}>
         <p>
           Created by Benjamin Vanlalvena. © {year}
           Created with <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
         </p>
-       </footer>
-    )
-  }
-
-  Footer.css = style
-  return Footer
-}) satisfies QuartzComponentConstructor
