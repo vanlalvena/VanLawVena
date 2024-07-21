2cf3764ebdd2d3538bcd0cb91c38f34b92f074ce
Benjamin Vanlalvena <vanlalvena@outlook.com>
2024-07-22 01:05:45 +0530

    Quartz sync: Jul 22, 2024, 1:05 AM

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -14,7 +14,7 @@ export default ((opts?: Options) => {
     return (
       <footer class={`${displayClass ?? ""}`}>
         <p>
-          {i18n(cfg.locale).components.footer.createdWith}{" "}
+          Created by Benjamin Vanlalvena. {i18n(cfg.locale).components.footer.createdWith}{" "}
           <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
         </p>
         <ul>
