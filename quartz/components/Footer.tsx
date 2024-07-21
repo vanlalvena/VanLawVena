c590ade7b318914ba65529f802cba15919bf58fc
Benjamin Vanlalvena <vanlalvena@outlook.com>
2024-07-22 01:57:07 +0530

    Quartz sync: Jul 22, 2024, 1:57 AM

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -10,20 +10,12 @@ interface Options {
 export default ((opts?: Options) => {
   const Footer: QuartzComponent = ({ displayClass, cfg }: QuartzComponentProps) => {
     const year = new Date().getFullYear()
+    const authorName = "Benjamin Vanlalvena"
+    const quartzUrl = "https://quartz.jzhao.xyz/"; 
     const links = opts?.links ?? []
     return (
-      <footer class={`${displayClass ?? ""}`}>
-        <p>
-          {i18n(cfg.locale).components.footer.createdWith}{" "}
-          <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
-        </p>
-        <ul>
-          {Object.entries(links).map(([text, link]) => (
-            <li>
-              <a href={link}>{text}</a>
-            </li>
-          ))}
-        </ul>
+      <footer>
+        Created by {authorName}  © {year} with{" "} <a href={quartzUrl}>Quartz</a> © {year}
       </footer>
     )
   }
