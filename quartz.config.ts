995bd10a6a4d9f4b63682feb8f18379e74f7d1c2
Benjamin Vanlalvena <vanlalvena@outlook.com>
2024-07-22 04:58:13 +0530

    Quartz sync: Jul 22, 2024, 4:58 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -15,7 +15,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "law.vanlalvena.in",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
